5a6d063b85567392029dd02530b6ca5d50197fe1
Keith Wu <FireWu@gmail.com>
2015-05-10 00:16:50 -0500

    cleaned up css and js files

--- app/assets/javascripts/content.js
+++ app/assets/javascripts/content.js
@@ -18,6 +18,25 @@ $(document).ready(function(){
     });
     
     $('.secondary-languages').sortable();
+
+
+    //For Released Review's drill down
+    if ($("#ReleasedTable").length > 0) {
+        $('.drill').on('click', function() {
+            $('.activeDrill').removeClass("activeDrill").addClass("drilled");
+            parent = $(this).parent().parent();
+            if ((parent).hasClass('drilled')) {
+                //Need to unmark it and remove div
+                parent.removeClass('drilled');
+                parent.next().remove();
+            } 
+            else {
+                parent.addClass("activeDrill");    
+                params = {id: parent.attr("class")};
+                $.get('/maintenance/released_review_drill', params);
+            }  
+        });
+    };
     
 });
 
